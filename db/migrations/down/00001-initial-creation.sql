25087351be7c07db7c2b2efb52e0c165ff63f19c
Christopher Mark Gore <cgore@cgore.com>
2011-09-21 01:22:48 -0500

    Adding up/down migrations.

---
+++ db/migrations/down/00001-initial-creation.sql
@@ -0,0 +1,11 @@
+-- Copyright (c) 2005 -- 2011, Christopher Mark Gore, all rights reserved.
+-- 8729 Lower Marine Road, Saint Jacob, Illinois 62281 USA.
+-- Phone: +1 (573) 452-3216
+-- Web: http://www.cgore.com
+-- Email: cgore@cgore.com
+
+-- This creates the trading database.
+-- This assumes PostgreSQL 9.1 or better.
+
+drop database livermore;
+drop user livermore;
