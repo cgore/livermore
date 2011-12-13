77c63239c0e14c2528d0c5a3a6e201bc52f13d80
Christopher Mark Gore <cgore@cgore.com>
2011-12-12 18:03:51 -0600

    Copyright changeover to BSD-style.

--- animat-xcs.lisp
+++ animat-xcs.lisp
@@ -1,23 +1,52 @@
 ;;;; Copyright (c) 2005 -- 2011, Christopher Mark Gore, all rights reserved.
+;;;;
 ;;;; 8729 Lower Marine Road, Saint Jacob, Illinois 62281 USA.
-;;;; Phone: +1 (573) 452-3216
 ;;;; Web: http://cgore.com
 ;;;; Email: cgore@cgore.com
+;;;;
+;;;; Redistribution and use in source and binary forms, with or without
+;;;; modification, are permitted provided that the following conditions are met:
+;;;;
+;;;;     * Redistributions of source code must retain the above copyright
+;;;;       notice, this list of conditions and the following disclaimer.
+;;;;
+;;;;     * Redistributions in binary form must reproduce the above copyright
+;;;;       notice, this list of conditions and the following disclaimer in the
+;;;;       documentation and/or other materials provided with the distribution.
+;;;;
+;;;;     * Neither the name of Christopher Mark Gore nor the names of other
+;;;;       contributors may be used to endorse or promote products derived from
+;;;;       this software without specific prior written permission.
+;;;;
+;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;;; POSSIBILITY OF SUCH DAMAGE.
 
 (require :asdf)
 (asdf:load-system "cgore-utilities")
+(asdf:load-system "cgore-evolutionary-algorithms")
 
-(load "xcs")
-(in-package :xcs)
-(use-package '("COMMON-LISP" "UTILITIES"))
-(export '(animat-analyzer
-           current-situation
-           initialize
-           get-situation
-           execute-action
-           get-reward
-           end-of-problem?
-           start-animat-experiment))
+(defpackage :cgore-anamat-xcs
+  (:use :common-lisp
+        :cgore-utilities
+        :cgore-xcs)
+  (:export :animat-analyzer
+           :current-situation
+           :initialize
+           :get-situation
+           :execute-action
+           :get-reward
+           :end-of-problem?
+           :start-animat-experiment))
+(in-package :cgore-anamat-xcs)
 
 (load "animat-xcs-parameters.lisp")
 
@@ -86,8 +115,8 @@
 
 (defmethod initialize ((analyzer animat-analyzer))
   "Reads the file given by file-name into the world array"
-  (with-slots (world-width 
-	       world-height 
+  (with-slots (world-width
+	       world-height
 	       world file-name
 	       multistep?) analyzer
     (setf multistep? t)
@@ -96,7 +125,7 @@
 		      :element-type 'character)
       (setf world-width (read stream))
       (setf world-height (read stream))
-      
+
       (setf world (make-array (list world-width world-height)))
       (loop for i from 0 to (1- world-height) do
 	    (loop for j from 0 to (1- world-width) do
@@ -139,15 +168,15 @@
 
 (defmethod multistep? ((analyzer animat-analyzer))
   t)
-		      
+
 (defmethod get-situation ((analyzer animat-analyzer))
   "Get the current-situation which is represented by a 24-element
    truth list, which contains the 3-element sensor codes for the 8
    locations nearest the current location. The codes are arranged in
    counter-clockwise order starting from directly north. Boundaries
    wrap around to the other side."
-  (with-slots (current-situation 
-	       world 
+  (with-slots (current-situation
+	       world
 	       x-location y-location
 	       world-width world-height) analyzer
     (setf current-situation nil)
@@ -162,12 +191,12 @@
 	(nconcf current-situation
 		(sensor-code (aref world new-x new-y)))))
     current-situation))
-	
+
 (defmethod execute-action ((analyzer animat-analyzer) (action integer))
   "Moves the animat in the given direction. Edges wrap around to the
    other side, and trying to move into a rock is simply a null
    operation."
-  (with-slots (world 
+  (with-slots (world
 	       x-location y-location
 	       world-width world-height
 	       current-action current-step-count) analyzer
@@ -212,7 +241,7 @@
 (defmethod get-reward ((analyzer animat-analyzer))
   "Get reward based on the current location of the animat. It gets
    a reward of 1000 for being on food and a reward of 0 for all else."
-  (with-slots (world 
+  (with-slots (world
 	       x-location y-location
 	       current-step-count
 	       number-of-problems) analyzer
