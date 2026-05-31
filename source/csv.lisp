c65037ad7530117bf816cc7bc019a067b12e3e5d
Christopher Mark Gore <cgore@cgore.com>
2026-05-31 17:59:32 -0500

    Cleanup to csv.lisp so it loads and works successfully

--- source/csv.lisp
+++ source/csv.lisp
@@ -1,8 +1,8 @@
-;;;; Copyright (c) 2005 -- 2014, Christopher Mark Gore,
+;;;; Copyright (c) 2005 -- 2026, Christopher Mark Gore,
 ;;;; Soli Deo Gloria,
 ;;;; All rights reserved.
 ;;;;
-;;;; 2317 South River Road, Saint Charles, Missouri 63303 USA.
+;;;; 22 Forest Glade Court, Saint Charles, Missouri 63304 USA.
 ;;;; Web: http://cgore.com
 ;;;; Email: cgore@cgore.com
 ;;;;
@@ -33,12 +33,10 @@
 ;;;; POSSIBILITY OF SUCH DAMAGE.
 
 (require :asdf)
-(asdf:load-system "cgore-utilities")
 
-(unless (find-package 'utilities) (load "utilities/utilities"))
 (unless (find-package 'csv)
   (defpackage :csv
-    (:use :common-lisp :utilities)
+    (:use :common-lisp)
     (:export :*separator*
              :*quote*
              :character-list-from-stream
